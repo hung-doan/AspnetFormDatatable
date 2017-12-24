f02bbabaa9d161502dea6d6f98f43c11c57b139a
Hung Doan <hung.doantan@gmail.com>
2017-12-24 13:49:39 +0700

    Init

---
+++ AspnetFormDatatable.Test/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspnetFormDatatable.Test.Startup))]
+namespace AspnetFormDatatable.Test
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
